421148a6da11cc3422859b7f94ac0404b4fd7c20
slawomir <kozokslawomir@gmail.com>
2018-10-22 23:12:36 +0200

    Changed level loader due to changes in parsing methods.

--- level/levelloader.go
+++ level/levelloader.go
@@ -1,6 +1,27 @@
 package level
 
+import (
+	"strings"
+	"unicode"
+)
+
 //Loader for level interface
 type Loader interface {
+	Parse(data Data)
+	ParseLine(line string)
 	Load(path string)
+}
+
+/*//Parse data for level
+func (level Level) Parse(data Data) {
+	for element in
+}*/
+
+func (level Level) ParseLine(line string) {
+	strings.Map(func(element rune) rune {
+		if unicode.IsNumber(element) || element == ',' {
+			return element
+		}
+		return -1
+	}, line)
 }
